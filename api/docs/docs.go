2650280c9fd812d266eb1b4dc40da2b7458f8c40
SaidovZohid <zohidsaidov02122004@gmail.com>
2022-12-25 17:25:07 +0500

    updated swagger

--- api/docs/docs.go
+++ api/docs/docs.go
@@ -984,7 +984,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "2.0",
-	Host:             "localhost:8080",
+	Host:             "",
 	BasePath:         "/v1",
 	Schemes:          []string{},
 	Title:            "",
